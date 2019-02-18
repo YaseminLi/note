22baa3414ead365dadf39a3934644dacaaee9a47
Yasemin <yaseminli@163.com>
2019-02-18 13:58:58 +0800

    未完持续

--- node/demos/43_promisify.js
+++ node/demos/43_promisify.js
@@ -1,9 +1,9 @@
 //回调地狱 异步里面调异步
 
 //方法一：readfile写法
-const fs=require('fs');
-const promisify=require('util').promisify;
-const read=promisify(fs.readFile);
+const fs = require('fs');
+const promisify = require('util').promisify;
+const read = promisify(fs.readFile);
 
 // read('./43_promisify.js').then((data)=>{
 //     console.log(data.toString());
@@ -12,3 +12,13 @@ const read=promisify(fs.readFile);
 // });
 
 //方法二：async await
+async function test() {
+    try {
+        const tem = await read('./433_promisify.js');
+        console.log(tem.toString());
+    }
+    catch (err) {
+        console.log(err);
+    }
+};
+test();
