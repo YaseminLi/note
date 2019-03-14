7b9cfdf33dd0740edf17e942cb27962b0f874667
YaseminLi <yaseminli@163.com>
2019-03-14 13:11:59 +0800

    本地构建gulp

---
+++ node/building/gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const del = require('del');
+const autoprefxer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+function clean(done) {
+    del.sync('build');
+    done();
+}
+
+function handleless(done) {
+    gulp.src(['src/**/*.less'])
+        .pipe(less())
+        .pipe(autoprefxer({
+            browsers: ['last 5 versions', 'Firefox>20'],
+            cascade: false//是否美化属性
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build'));
+    done();
+};
+function move(done) {
+    gulp.src(['src/**/*'])
+        .pipe(gulp.dest('build'));
+    done();
+}
+
+
+function watch(done){
+    const watcher = gulp.watch('src/**/*.js');
+    watcher.on('change', function (path, stats) {
+        console.log(`File ${path} was changed`);
+    });
+
+    watcher.on('add', function (path, stats) {
+        console.log(`File ${path} was added`);
+    });
+
+    watcher.on('unlink', function (path, stats) {
+        console.log(`File ${path} was removed`);
+    });
+
+    //   watcher.close();
+  
+};
+
+exports.default = gulp.series(clean, handleless, move);
+exports.move = move;
+exports.watch = watch;
+
+
